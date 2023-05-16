4d2826800530a72a67272382baca037e667784f1
Muhammad Ali Tanveer <tanveer7652@gmail.com>
2023-05-16 07:04:15 -0700

    PlayFab GSDK Added

--- Source/VRBowling/VRBowling.Build.cs
+++ Source/VRBowling/VRBowling.Build.cs
@@ -8,7 +8,7 @@ public class VRBowling : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PlayFabGSDK" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
